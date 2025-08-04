39fae0fa2dd740b673a6ad69d02999c80aa48473
Math3uso <matheusfred955@gmail.com>
2025-08-04 15:12:58 -0300

    feat(auth): add JWT plugin to Fastify

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,14 @@ import cors from "@fastify/cors";
 export const app = fastify();
 
 app.register(fastifyJwt, {
-    secret: "secret"
+    secret: env.JWT_SECRET,
+    cookie: {
+        cookieName: 'refreshToken',
+        signed: false
+    },
+    sign: {
+        expiresIn: "10m"
+    }
 });
 
 app.register(appRoutes);
@@ -16,6 +23,10 @@ app.register(appRoutes);
 app.register(cors, {
     origin: env.NODE_ENV === "dev" ? "*" : "definir url",
     methods: ["GET", "POST", "PUT", "DELETE"],
+});
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET
 });
 
 app.setErrorHandler((error, _, reply) => {
