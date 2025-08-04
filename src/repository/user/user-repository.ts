5f172c67248a93a33eee8744f97dfe86f3311c2e
Math3uso <matheusfred955@gmail.com>
2025-08-04 15:07:28 -0300

    chore: create user repository and interface

---
+++ src/repository/user/user-repository.ts
@@ -0,0 +1,34 @@
+import { Prisma, User } from "@prisma/client";
+import { IUserRepository } from "./i-user-repository";
+import { prisma } from "@/lib/prisma";
+
+export class UserRepository implements IUserRepository {
+    async setRefrashToken(userId: string, token: string): Promise<User | null> {
+
+        const user = await prisma.user.update({
+            where: { id: userId },
+            data: {
+                refrash_token: token
+            }
+        });
+
+        return user;
+    }
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+        const user = await prisma.user.create({ data });
+        return user;
+    }
+    async findById(id: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({
+            where: { id }
+        });
+        return user;
+    }
+    async findByEmail(email: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({
+            where: { email }
+        });
+        return user;
+    }
+
+}
