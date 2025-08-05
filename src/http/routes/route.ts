ce3af64dcf73336597102c529201a17cca322781
Math3uso <matheusfred955@gmail.com>
2025-08-05 13:52:43 -0300

    update:add verifyJwt to the fetch/play routes

--- src/http/routes/route.ts
+++ src/http/routes/route.ts
@@ -6,6 +6,7 @@ import { channeIdlController } from "../controllers/channel/channel-id";
 import { GetListByKeyword } from 'youtube-search-api';
 import { ChannelInfo, PlaylistInfo, VideoInfo } from "@/@types/medias";
 import { searchControllerV2 } from "../controllers/search/search_v2";
+import { verifyJwt } from "../middlewares/verify-jwt";
 
 
 export async function appRoutes(app: FastifyInstance) {
@@ -13,22 +14,22 @@ export async function appRoutes(app: FastifyInstance) {
         return reply.status(200).send({ message: "foi" });
     });
 
-    app.get("/search/:name", searchController);
-    app.get("/search/v2/:name", searchControllerV2);
-    app.get("/play", playController);
-    app.get("/chennel/:id", channeIdlController);
+    app.get("/search/:name", { onRequest: [verifyJwt] }, searchController);
+    app.get("/search/v2/:name", { onRequest: [verifyJwt] }, searchControllerV2);
+    app.get("/play", { onRequest: [verifyJwt] }, playController);
+    app.get("/chennel/:id", { onRequest: [verifyJwt] }, channeIdlController);
 
-    app.get("/teste2/:name", async (req, reply) => {
+    // app.get("/teste2/:name", async (req, reply) => {
 
-        const { name } = req.params as { name: string };
+    //     const { name } = req.params as { name: string };
 
-        const { videoInfo } = await YTMedia.getListVideos({ name });
+    //     const { videoInfo } = await YTMedia.getListVideos({ name });
 
-        const playList = await YTMedia.getPlaylists({ name, maxResult: 5 });
+    //     const playList = await YTMedia.getPlaylists({ name, maxResult: 5 });
 
-        return reply.status(200).send({
-            videoInfo,
-            playList
-        });
-    });
+    //     return reply.status(200).send({
+    //         videoInfo,
+    //         playList
+    //     });
+    // });
 }
