73a65dba26092699d01e08b093cb81d04660b419
Math3uso <matheusfred955@gmail.com>
2025-08-04 21:36:33 -0300

    update: returning user in profile route

--- src/http/controllers/user/profile.ts
+++ src/http/controllers/user/profile.ts
@@ -1,6 +1,22 @@
+import { UserIsNoteFoundError } from "@/services/errors/user-is-not-found-error";
+import { MakeProfileService } from "@/services/factory/make-profile-service";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
 
-    return reply.status(200).send();
+    const profileService = MakeProfileService();
+
+    const userId = request.user.sub;
+
+    try {
+
+        const { user } = await profileService.execute({ userId })
+
+        return reply.status(200).send({ user });
+
+    } catch (error) {
+        if (error instanceof UserIsNoteFoundError) {
+            return reply.status(404).send({ message: error.message });
+        }
+    }
 }
