a7f4b19d5b1bb537fd98cbb15ba6f52b1eeb62ff
Math3uso <matheusfred955@gmail.com>
2025-11-04 23:02:42 -0300

    feat: create UpdateSessionService

---
+++ src/services/session/update-session-service.ts
@@ -0,0 +1,24 @@
+import { ISessionRepository } from "@/repository/session/i-session.repository";
+
+interface UpdateSessionServiceRequest {
+    token: string;
+    newToken: string;
+}
+
+export class UpdateSessionService {
+    constructor(private readonly sessionRepository: ISessionRepository) { }
+
+    async execute({ token, newToken }: UpdateSessionServiceRequest) {
+        const session = await this.sessionRepository.findByToken(token);
+
+        if (!session) throw new Error('Session not found');
+
+        session.refrashToken = newToken;
+
+        await this.sessionRepository.update(session);
+
+        return {
+            session
+        }
+    }
+}
