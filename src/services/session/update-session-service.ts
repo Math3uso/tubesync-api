f02cda09bf91ac5f53b143655adaaf87d40592a3
Math3uso <matheusfred955@gmail.com>
2025-11-09 23:40:53 -0300

    refactor(session): rewrite refresh token service

--- src/services/session/update-session-service.ts
+++ src/services/session/update-session-service.ts
@@ -1,25 +1,35 @@
 import { NotFoundExeption } from "@/@exceptions/default/not-found.exeption";
 import { ISessionRepository } from "@/repository/session/i-session.repository";
+import { SessionRepository } from "@/repository/session/session-repository";
 
 interface UpdateSessionServiceRequest {
     token: string;
-    newToken: string;
+    //newToken: string;
+    generateToken: (payload: object) => Promise<string>;
 }
 
 export class UpdateSessionService {
     constructor(private readonly sessionRepository: ISessionRepository) { }
 
-    async execute({ token, newToken }: UpdateSessionServiceRequest) {
+    async execute({ token, generateToken }: UpdateSessionServiceRequest) {
         const session = await this.sessionRepository.findByToken(token);
 
         if (!session) throw new NotFoundExeption('Session not found');
+
+        const newToken = await generateToken({ sub: session.userId });
 
         session.refrashToken = newToken;
 
         await this.sessionRepository.update(session);
 
         return {
-            session
+            session,
+            newToken
         }
+    }
+
+    static create() {
+        const sessionRepository: ISessionRepository = new SessionRepository();
+        return new UpdateSessionService(sessionRepository);
     }
 }
