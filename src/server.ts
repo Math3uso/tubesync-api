18aff7188e2abb0ccc240e388cff967040c419e7
Math3uso <matheusfred955@gmail.com>
2026-04-30 22:18:04 -0300

    feat: handling startup errors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import { app } from "./app";
 import { env } from "./env";
+import { prisma } from "./lib/prisma";
 
-app.listen({
-    host: '0.0.0.0',
-    port: env.PORT
-}).then(() => {
-    console.log(`[INFO] server is run http://localhost:${env.PORT}`);
-});
+async function bootstrap() {
+    try {
+        await prisma.$connect();
+
+        await app.listen({
+            host: '0.0.0.0',
+            port: env.PORT
+        });
+
+        console.log(`[INFO] server is run http://localhost:${env.PORT}`);
+    } catch (error) {
+        console.error("[ERROR] failed to start server due to database connection error.", error);
+        process.exit(1);
+    }
+}
+
+bootstrap();
